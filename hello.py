62f5ed1d57c1fde6a9189c6361502c86e42adbea
Emily Klyza <eklyza@gmail.com>
2015-03-18 22:34:10 -0700

    added in username

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Good morning, Emmmily!"
+@app.route("/<username>")
+def hello(username):
+	return "Good morning, "+username+"!"
 
 if __name__ == "__main__":
 	app.run()
